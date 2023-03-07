5cc1f7e3e1a1885451b35212d556249f35e18543
Nese84 <nbserbest84@gmail.com>
2023-03-07 20:53:17 +0300

    AppRouter

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    <
+    
+    
+    
+    
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
