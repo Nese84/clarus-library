08e330904874187d46732c51940a8d4e73dd8912
Nese84 <nbserbest84@gmail.com>
2023-03-07 22:34:50 +0300

    Navbar.style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Nav } from "./Navbar.style";
+import { Logo, Menu, MenuLink, Nav } from "./Navbar.style";
 
 const Navbar = () => {
-  return 
-  <Nav>
-    <Logo to"/"
+  return (
+    <Nav justify="space-between">
+      <Logo to="/">Clarus Library</Logo>
+      <Menu>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="/about">About</MenuLink>
+        <MenuLink to="/register">Register</MenuLink>
+        <MenuLink to="/login">About</MenuLink>
 
-  </Nav>;
+      </Menu>
+    </Nav>
+  );
 };
 
 export default Navbar;
